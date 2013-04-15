1dba9f5d5de3fded34a52546ee43da18d2876cbb
Diego Fernandez Slezak <dfslezak@gmail.com>
2013-04-15 16:20:26 -0300

    Fixes #34 - Replaced verbose names with ugettext version.
    Following django translation manual, all strings of model names where
    replaced with ugettext version.
    
    After replacing each of them, .po file was created by running:
    django-admin.py makemessages -l es_AR

--- matemarote/users/models.py
+++ matemarote/users/models.py
@@ -1,10 +1,11 @@
+from django.utils.translation import ugettext as _
 from django.db import models
 
 # Create your models here.
 class UserProfile(models.Model):
-    birthdate = models.DateField("Fecha de nacimiento")
-    gender = models.SmallIntegerField("Sexo",choices=[(1,'hombre'),(2,'mujer')])
-    handedness = models.SmallIntegerField("Mano mas habil",choices=[(1,'derecha'),(2,'izquierda'),(3,'ambas')])
-    siblingnumber = models.PositiveIntegerField("Hermanos en total",default=0);
-    siblingorder = models.PositiveIntegerField("Tu orden de hermandad (1 es el mas grande)",default=0);
-    avatar = models.ImageField("tu avatar",upload_to='avatares',default='avatares/pepe.gif');
+    birthdate = models.DateField(_("Birth date"))
+    gender = models.SmallIntegerField(_("Sex"),choices=[(1,_("Male")),(2,_("Fermale"))])
+    handedness = models.SmallIntegerField(_("Handedness"),choices=[(1,_("Right")),(2,_("Left")),(3,_("Both"))])
+    siblingnumber = models.PositiveIntegerField(_("Sibling number"),default=0);
+    siblingorder = models.PositiveIntegerField(_("Sibling order (1 being the oldest)"),default=0);
+    avatar = models.ImageField(_("Avatar"),upload_to='avatares',default='avatares/pepe.gif');
