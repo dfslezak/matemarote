188381e1e130bb0db161cec912882369c50cf8c6
Diego Fernandez Slezak <dfslezak@gmail.com>
2013-04-18 17:41:51 -0300

    Created Game and GameFlow models. In GameFlow model, the graph must be an XML string with graph representation. Using signals, this string must be decoded when class is instantiated.

--- matemarote/games/models.py
+++ matemarote/games/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from pygraph.classes.digraph import digraph
 # Create your models here.
+
+class Game(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    
+class GameFlow(models.Model):
+    xml_graph = models.TextField()
