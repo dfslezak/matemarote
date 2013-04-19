5535fa7e2e7fb0c2532184d244d805e60a79f669
Diego Fernandez Slezak <dfslezak@gmail.com>
2013-04-18 23:50:48 -0300

    Created GameFlowLevel model. Now looking how to create a validator for nesting levels dependencies...

--- matemarote/games/models.py
+++ matemarote/games/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 class Game(models.Model):
     name = models.CharField(max_length=255)
@@ -8,7 +9,19 @@ class GameRevision(models.Model):
     game = models.ForeignKey(Game)
     version = models.IPAddressField()
     creation_date = models.DateTimeField()
-    previous_version = models.ForeignKey('self')
+    previous_version = models.ForeignKey('self',null=True)
+    
+class GameFlowNode(models.Model):
+    def clean(self):
+        level_diff = [((self.skill_level - xxx.skill_level) == 1) for xxx in self.previous_level_dep.objects.all()]
+        print level_diff
+        if False in level_diff:
+            raise ValidationError(u'%s is not an even number' % value)
+        
+    game_revision = models.ForeignKey(GameRevision)
+    skill_level = models.IntegerField()
+    previous_level_dep = models.ManyToManyField('self')
     
 #class GameFlow(models.Model):
+    
 #    xml_graph = models.TextField()
