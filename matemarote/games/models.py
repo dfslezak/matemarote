7257aea6144ad82e5193154afccc9a9b94f17be7
Diego Fernandez Slezak <dfslezak@gmail.com>
2013-04-18 23:00:43 -0300

    Refs #41 - Created game revision class based on OLPC impl.
    Gameflow indicates which game and which game version are available
    for playing. Then, a GameRevision model was creating for admin
    versioning of games.

--- matemarote/games/models.py
+++ matemarote/games/models.py
@@ -1,10 +1,14 @@
 from django.db import models
-from pygraph.classes.digraph import digraph
-# Create your models here.
 
 class Game(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+
+class GameRevision(models.Model):
+    game = models.ForeignKey(Game)
+    version = models.IPAddressField()
+    creation_date = models.DateTimeField()
+    previous_version = models.ForeignKey('self')
     
-class GameFlow(models.Model):
-    xml_graph = models.TextField()
+#class GameFlow(models.Model):
+#    xml_graph = models.TextField()
