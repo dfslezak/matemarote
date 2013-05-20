4a032fec4378c3eb02e570da0b8437e379a0b44f
Diego Fernandez Slezak <dfslezak@gmail.com>
2013-05-20 15:08:31 -0300

    Conflicts: 	matemarote/users/models.py

--- matemarote/users/models.py
+++ matemarote/users/models.py
@@ -11,6 +11,7 @@ class UserProfile(models.Model):
     handedness = models.SmallIntegerField(_("Handedness"),choices=[(1,_("Right")),(2,_("Left")),(3,_("Both"))])
     siblingnumber = models.PositiveIntegerField(_("Sibling number"),default=0);
     siblingorder = models.PositiveIntegerField(_("Sibling order (1 being the oldest)"),default=0);
+<<<<<<< HEAD
     avatar = models.ImageField(_("Avatar"),upload_to='avatares',null=True);
 
     program = models.ForeignKey('Program')
@@ -22,3 +23,17 @@ class Program(models.Model):
 
     parent = models.ForeignKey('self', null=True)
     gameflow = models.ForeignKey('GameFlow', null=True)
+=======
+    avatar = models.ImageField(_("Avatar"),upload_to='avatares',default='avatares/pepe.gif');
+
+	program = models.ForeignKey('Program')
+
+class Program(models.Model):
+	name = models.CharField(max_length=150)
+	#language = models.SmallIntegerField(_("Language"),choices=[(1,_("Spanish")),(2,_("English"))])
+	language = models.CharField(choices=LANGUAGES)
+	
+	parent = models.ForeignKey('self', null=True)
+	gameflow = models.ForeignKey('GameFlow', null=True)
+	
+>>>>>>> mlopez/users
