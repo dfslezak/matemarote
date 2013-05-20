d080c69a01564fc693040153396983cb86f07232
Diego Fernandez Slezak <dfslezak@gmail.com>
2013-05-20 15:06:31 -0300

    solved conflict at matemarote/users/models.py changed avatar line accepting null images.

--- matemarote/users/models.py
+++ matemarote/users/models.py
@@ -1,4 +1,5 @@
 from django.utils.translation import ugettext as _
+from django.conf.global_settings import LANGUAGES
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -11,3 +12,13 @@ class UserProfile(models.Model):
     siblingnumber = models.PositiveIntegerField(_("Sibling number"),default=0);
     siblingorder = models.PositiveIntegerField(_("Sibling order (1 being the oldest)"),default=0);
     avatar = models.ImageField(_("Avatar"),upload_to='avatares',null=True);
+
+    program = models.ForeignKey('Program')
+
+class Program(models.Model):
+    name = models.CharField(max_length=150)
+    #language = models.SmallIntegerField(_("Language"),choices=[(1,_("Spanish")),(2,_("English"))])
+    language = models.CharField(choices=LANGUAGES)
+
+    parent = models.ForeignKey('self', null=True)
+    gameflow = models.ForeignKey('GameFlow', null=True)
