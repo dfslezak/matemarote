679866b191d5c71b3971c6f96aaa20466779d4fb
Diego Fernandez Slezak <dfslezak@gmail.com>
2013-05-23 15:18:45 -0300

    Fixes #61 #62 #63 - Created home, view-profile and edit profile pages
    Created new view-profile template with button for edition values
    Linked username to profile in navigation bar.
    
    Added media and static files in URLs
    
    Added jquery for datepicker, used in profile edition.
    Added a starting index page with information about project.

--- matemarote/matemarote/urls.py
+++ matemarote/matemarote/urls.py
@@ -1,32 +1,32 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from users.models import UserProfile
 import registration
 from registration.forms import RegistrationFormUniqueEmail
+from django.conf.urls.static import static
+
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'matemarote.views.home', name='home'),
-    # url(r'^matemarote/', include('matemarote.foo.urls')),
     url(r'^gameflow/', 'games.views.gameflow'),
-    #url(r'^accounts/login/$', 'django.contrib.auth.views.login',{'template_name': 'login_local.html'}),
-    #url(r'^accounts/logout/$', 'django.contrib.auth.views.logout',{'template_name': 'logout.html'}),
     url(r'^$', 'web_frontend.views.web'),
 
+    url(r'^accounts/profile/$', 'web_frontend.views.view_profile'),
+    url(r'^accounts/profile/edit$', 'web_frontend.views.edit_profile'),
     url(r'^accounts/register/$', 'registration.views.register',
     {'form_class': RegistrationFormUniqueEmail,
      'backend': 'registration.backends.default.DefaultBackend'},       
      name='registration_register'),
     url(r'^accounts/', include('registration.backends.default.urls')),
-    #url(r'^accounts/', include('django.contrib.auth.urls')),
-    #url(r'', include('django.contrib.auth.urls')),
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin_tools/', include('admin_tools.urls')),
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 )
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- matemarote/web_frontend/views.py
+++ matemarote/web_frontend/views.py
@@ -2,11 +2,59 @@
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response,redirect
 from django.core.context_processors import csrf
+from users.models import UserProfile,UserProfileForm
+from django.forms.models import modelform_factory
 
 def web(request):
     c = RequestContext(request)
     c.update(csrf(request))
-    # ... view code here
-    return render_to_response('base.html', c)
+    return render_to_response('index/index.html', c)
+
+@login_required
+def view_profile(request):
+    c = RequestContext(request)
+    
+    u = request.user
+    profile = request.user.get_profile()
+    c['profile'] = profile
+    print profile.__dict__
+
+    return render_to_response('userprofile/view-profile.html', c)
+
+@login_required
+def edit_profile(request):
+    c = RequestContext(request)
+    c.update(csrf(request))
+    
+    if request.method == 'POST': # If the form has been submitted, save and redirect.
+        #print '---------- SAVE AND REDIRECT -----------'
+        form = UserProfileForm(data=request.POST,files=request.FILES,instance=request.user.get_profile()) 
+        if form.is_valid():
+            form.save()
+            
+            template = 'userprofile/view-profile.html'
+            profile = request.user.get_profile()
+            c['profile'] = profile
+
+        else:
+            #print form.errors
+            #print form.__dict__
+            c['form'] = form
+            template = 'userprofile/edit-profile.html'
+
+    else: # Show form for edition
+        #print '---------- SHOW FOR EDITION -----------'
+        #print 'User: ', request.user
+        u = request.user
+        profile = request.user.get_profile()
+        #print 'Profile: ', profile
+        
+        form = UserProfileForm(instance=profile)
+        #print 'Form: ', form.as_p()
+        c['form'] = form
+        template = 'userprofile/edit-profile.html'
+
+    return render_to_response(template, c)
+    
