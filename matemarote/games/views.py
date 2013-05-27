c00211ba5bb02bbc60d29e773b7152978273e6ef
Diego Fernandez Slezak <dfslezak@gmail.com>
2013-05-26 23:59:33 -0300

    arrancando con la impresion del gameflow.

--- matemarote/games/views.py
+++ matemarote/games/views.py
@@ -1,13 +1,22 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
-from users.models import *
-from datetime import date
-# Create your views here.
+from django.http import HttpResponse,Http404
+from django.template import RequestContext
+
+from django.shortcuts import render_to_response,redirect
+from django.core.context_processors import csrf
 
 @login_required
 def gameflow(request):
-    (us,created) = UserProfile.objects.get_or_create(user=request.user,defaults={'birthdate': date(1970, 01, 01),'gender':1,'handedness':1})
-    u = us.user
+    c = RequestContext(request)
 
-    html = "<html><body>User: %s<br><br>GameFlows: %s</body></html>" % (u,us.program.game_flow.list_games_per_skill(us.game_flow_status))
-    return HttpResponse(html)
+    user = request.user.get_profile()
+    gf = user.get_gameflow()
+    
+    sk = gf.list_games_per_skill(user.game_flow_status)
+    sk_list = []
+    
+    for s in sorted(sk.keys()):
+        sk_list.append(sk[s])
+    c['skill_list'] = sk_list
+    
+    return render_to_response('games/gameflow.html', c)
